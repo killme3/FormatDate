681bf8613a38c035792b6e62c5887476de34633b
yana mulyana <fulan@MacBook-Pro-yana.local>
2018-06-24 20:11:41 +0700

    first commit & add class FormatDate

---
+++ FormatDate/FormatDate.swift
@@ -0,0 +1,29 @@
+//
+//  FormatDate.swift
+//  FormatDate
+//
+//  Created by yana mulyana on 24/06/18.
+//  Copyright © 2018 yana mulyana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class FormatDate {
+    
+    public func formattedDateFromString(dateFormat: String, dateString: String, withFormat format: String) -> String? {
+        
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = dateFormat
+        
+        if let date = inputFormatter.date(from: dateString) {
+            
+            let outputFormatter = DateFormatter()
+            outputFormatter.dateFormat = format
+            
+            return outputFormatter.string(from: date)
+        }
+        
+        return nil
+    }
+}
